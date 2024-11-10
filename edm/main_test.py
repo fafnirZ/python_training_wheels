d19234e5747d0226b1db9376e49c014cb320fb02
fafnir <gokurohono@gmail.com>
2024-11-10 12:00:10 +1100

    feat: update

--- edm/main_test.py
+++ edm/main_test.py
@@ -6,30 +6,20 @@ import hypothesis.strategies as st
 import numpy as np
 
 # Define a strategy for generating n x 2 matrices
+# USING INTEGERS, floats are annoying
 def random_n_by_2_matrix():
   MAX_Y = 1000000
   MAX_X = 2
-  return st.lists(st.lists(st.floats(min_value=-100, max_value=100), min_size=MAX_X, max_size=MAX_X),
+  return st.lists(st.lists(st.integers(min_value=-100, max_value=100), min_size=MAX_X, max_size=MAX_X),
                   min_size=1, max_size=MAX_Y).map(np.array)
 
 
-def generate_n_cases(n):
-  strategy = random_n_by_2_matrix()
-  return [
-    strategy.example()
-    for _ in range(n)
-  ]
 
-
-@pytest.mark.parametrize(
-  "case",
-  [
-    *generate_n_cases(100)
-  ]
-)
-def test_equality(case):
-  res_manual = point_distance_manual(case)
-  res_edm = point_distance(case)
-
-  # assert (res_edm==res_manual).all()
-  assert np.allclose(res_edm, res_manual) # allows for floating point errors 
+@pytest.mark.parametrize("num_trials", [5,10,100])
+@given(matrix=random_n_by_2_matrix())
+def test_equality(num_trials, matrix, benchmark):
+  for _ in range(num_trials):
+    res_manual = point_distance_manual(matrix)
+    res_edm = point_distance(matrix)
+    # assert (res_edm==res_manual).all()
+    assert np.allclose(res_edm, res_manual) # allows for floating point errors 
